8c0230abc0029253e338b6e0b471c5a84f66cec9
Oswald Calvario <oswald.calvario@gmail.com>
2020-05-02 20:58:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,20 @@
 #write your code here
 
-def countdown(n)
-  n = i
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
   end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+  
+  
